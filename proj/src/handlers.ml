1e9e5a19d9247c648661b1375a2b48062f69d09a
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2022-12-07 17:17:02 +0000

    Handlers

---
+++ proj/src/handlers.ml
@@ -0,0 +1,21 @@
+open Async_kernel
+open Async_unix
+
+let conn =
+  let host = "localhost" in
+  let db = "abc" in
+  let user = "sa" in
+  let password = "super(#)password" in
+  let port = Some 1433 in
+  Mssql.with_conn ~host ~db ~user ~password ?port
+
+let home _request =
+  let id =
+    Thread_safe.block_on_async_exn @@ fun () ->
+    conn (fun db -> Mssql.execute db "SELECT * FROM users;") >>| function
+    | row :: _ ->
+        let id = Mssql.Row.int_exn row "id" in
+        id
+    | _ -> assert false
+  in
+  Dream.html (Format.sprintf "<h1>%d</h1>" id)
