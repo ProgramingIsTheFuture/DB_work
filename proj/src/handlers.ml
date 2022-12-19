e4c7caaee19968fe10d41fe2657deb51d3eed359
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2022-12-19 14:13:26 +0000

    Helper functions?

--- proj/src/handlers.ml
+++ proj/src/handlers.ml
@@ -1,6 +1,8 @@
 open Async_kernel
 open Async_unix
 
+type user = { id : string; name : string } [@@deriving yojson]
+
 let conn =
   let host = "localhost" in
   let db = "abc" in
@@ -9,13 +11,17 @@ let conn =
   let port = Some 1433 in
   Mssql.with_conn ~host ~db ~user ~password ?port
 
-let home _request =
-  let id =
-    Thread_safe.block_on_async_exn @@ fun () ->
-    conn (fun db -> Mssql.execute db "SELECT * FROM users;") >>| function
-    | row :: _ ->
-        let id = Mssql.Row.int_exn row "id" in
-        id
-    | _ -> assert false
-  in
-  Dream.html (Format.sprintf "<h1>%d</h1>" id)
+let query q =
+  Thread_safe.block_on_async_exn @@ fun () ->
+  conn (fun db -> Mssql.execute db q) >>| function
+  | [] -> []
+  | rows -> List.map Mssql.Row.to_alist rows
+
+let () =
+  let data = query "SELECT * FROM users;" in
+  List.map (fun a -> yojson_of_list (fun (_, b2) -> yojson_of_string b2) a) data
+  |> List.map user_of_yojson
+  |> List.map (fun a -> Format.printf "%s %s" a.id a.name)
+  |> ignore
+
+let home _request = Dream.html (Format.sprintf "<h1>%d</h1>" 1)
