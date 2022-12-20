2896f95851f93a5dc02f0047c873b74c5441a3b9
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2022-12-20 22:34:07 +0000

    HTML Template

--- proj/src/home.eml.ml
+++ proj/src/home.eml.ml
@@ -10,13 +10,30 @@
 *  </html> *)
 
 
-let home= 
-<head>
-  <link rel="stylesheet" href="/static/assets/style.css">
-  <title>Gestão de Projetos UBI</title>
-</head>
+let base title bdy =
+  <html>
+    <head>
+      <link rel="stylesheet" href="/assets/style.css">
+      <title><%s title %></title>
+    </head>
+    <body>
+      <div id="root">
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"></script>
+      <script>
+        const pagehtml = `<%s bdy %>`;
+        const ConvertStringToHTML = function (str) {
+         let parser = new DOMParser();
+         let doc = parser.parseFromString(str, 'text/html');
+         return doc.body;
+        };
+        const page = ConvertStringToHTML(pagehtml);
+        document.getElementById("root").append(ConvertStringToHTML(page.childNodes[0].data));
+      </script>
+    </body>
+  </html>
 
-<body>
+let home = 
   <div class="container">
     <h1 class="title" style="text-align: center; padding: 30px">Gestão de Projetos UBI</h1>
     <nav>
@@ -29,8 +46,4 @@ let home=
         <li><a href="#">Option 6</a></li>
       </ul>
     </nav>
-  </div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"></script>
-</body>
-
-</html>
+  </div> 
--- proj/src/main.ml
+++ proj/src/main.ml
@@ -1,4 +1,7 @@
 let () =
   Dream.run @@ Dream.logger @@ Dream.memory_sessions
-  @@ Dream.router [ Dream.get "/" Handlers.home;
-                    Dream.get "/static/**" @@ Dream.static "." ] 
+  @@ Dream.router
+       [
+         Dream.get "/" Handlers.home;
+         Dream.get "/assets/**" @@ Dream.static "./assets";
+       ]
