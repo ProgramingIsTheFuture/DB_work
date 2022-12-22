b1f36f9617bafcdb5ef2e56d04073e15c894ff52
leonardo santos <leomendesantos@gmail.com>
2022-12-22 21:39:05 +0000

    templates

--- proj/src/main.ml
+++ proj/src/main.ml
@@ -3,9 +3,10 @@ let () =
   @@ Dream.router
        [
          Dream.get "/" @@ Handlers.home;
-         Dream.get "/projetos" @@ Handlers.serve (Pages.projetos (Handlers.query ("SELECT * FROM users;") |> Handlers.users_of_row)) "Projetos";
+         Dream.get "/projetos" @@ Handlers.serve (Pages.projetos_contratos (Handlers.query ("SELECT * FROM users;") |> Handlers.users_of_row)) "Projetos";
          (*Dream.get "/contratos"*)
          Dream.get "/investigadores" @@ Handlers.serve Pages.investigadores "Investigadores";
          Dream.get "/projetos/:id" @@ (fun request -> Dream.html (Dream.param request "id" ));
+         Dream.get "/inves_teste" @@ Handlers.serve Templates.inves_template "Investigadores";
          Dream.get "/assets/**" @@ Dream.static "./assets";
        ]
