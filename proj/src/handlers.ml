dba083fadfa8e38085c2ee09d40b318db3a59b75
leonardo santos <leomendesantos@gmail.com>
2022-12-26 22:59:01 +0000

    detalhes financiamento no porjeto e outras merdas

--- proj/src/handlers.ml
+++ proj/src/handlers.ml
@@ -32,19 +32,64 @@ let institute _req = serve institutes "Institutos"
 let entities _req = serve entities "Entidades"
 
 let projects_id req =
+  let id = (Dream.param req "id" |> int_of_string)
+  in
   let result =
     query
-      ~params:[ Mssql.Param.Int (Dream.param req "id" |> int_of_string) ]
+      ~params:[ Mssql.Param.Int id ]
       "SELECT * FROM projeto P WHERE id = $1"
     |> List.hd
   in
 let result2 =
     query
-      ~params:[ Mssql.Param.Int (Dream.param req "id" |> int_of_string) ]
-      "SELECT K.id, K.keyword FROM projeto P, keywords K WHERE P.id = $1 AND P.id \
+      ~params:[ Mssql.Param.Int id ]
+      "SELECT K.id, K.keyword FROM projeto P, keywords K \
+      WHERE P.id = $1 AND P.id \
        = K.projectId;"
   in
+let result3 =
+    query
+      ~params:[ Mssql.Param.Int id ]
+      "SELECT Pub.nomeJornal, Pub.id, Pub.url, Pub.doi FROM Projeto P, Publicacao Pub \
+      WHERE P.id = $1 AND P.id = Pub.projectId"
+  in
+let result4 =
+    query
+      ~params:[ Mssql.Param.Int id ]
+      "SELECT I.id as Iid, I.nome as Inome, Pl.designacao as papel \ 
+      FROM Projeto P \
+      INNER JOIN Participa PI ON P.id = PI.projectId \
+      INNER JOIN Investigador I ON PI.investigadorId = I.id \
+      INNER JOIN Papel Pl ON PI.papelId = Pl.id \
+      WHERE P.id = $1"
+  in
+let result5 =
+    query
+      ~params:[ Mssql.Param.Int (Dream.param req "id" |> int_of_string) ]
+      "SELECT DISTINCT AC.designacao as area, D.designacao as dominio FROM Projeto P \
+      INNER JOIN AreaProjeto AP ON P.id = AP.projectId \
+      INNER JOIN AreaCientifica AC ON AP.areaCientificaId = AC.id \
+      INNER JOIN Dominio D ON AC.dominioId = D.id \
+      WHERE P.id = $1"
+  in
+  serve (project result id result2 result3 result4 result5) "Projetos"
 
-  serve (project result result2) "Projetos"
+let projects_id_entities req =
+  let id = (Dream.param req "id" |> int_of_string)
+  in
+  let result =
+    query
+      ~params:[ Mssql.Param.Int id ]
+      "SELECT * FROM projeto P WHERE id = $1"
+    |> List.hd
+  in
+  let result2 =
+    query
+      ~params:[ Mssql.Param.Int (Dream.param req "id" |> int_of_string) ]
+      "SELECT C.id, C.nome FROM Contrato C \
+      INNER JOIN Projeto P ON C.projectId = P.id \
+      WHERE P.id = $1"
+  in
+  serve (project_entities result result2) "Projetos"
 
 let inves_test _req = serve "" "Investigadores"
--- proj/src/main.ml
+++ proj/src/main.ml
@@ -8,6 +8,7 @@ let () =
          Dream.get "/institutos" @@ Handlers.institute;
          Dream.get "/entidades" @@ Handlers.entities;
          Dream.get "/projetos/:id" @@ Handlers.projects_id;
+         Dream.get "/projetos/:id/financiamento" @@ Handlers.projects_id_entities;
          Dream.get "/inves_teste" @@ Handlers.inves_test;
          Dream.get "/assets/**" @@ Dream.static "./assets";
        ]
