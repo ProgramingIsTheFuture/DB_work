b1d137994eb036e0c82c3ecfc19b14eebba8abed
leonardo santos <leomendesantos@gmail.com>
2022-12-28 01:23:01 +0000

    programas e unidades

---
+++ proj/src/templates/prog.eml.ml
@@ -0,0 +1,91 @@
+open Types
+
+let programas (progs: data list) =
+  General.navbar_inpage "Programas" ^
+  <div style="text-align: center; width: 1000px; margin: 0 auto; border-style: none; margin-top: 30px;">
+    <table class="table table-dark table-hover">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider">
+        <% progs |> List.iter begin fun x -> %> 
+        <tr>
+          <th scope="row"><%s x<|"id" %></th>
+          <td><a href='/programas/<%s x<|"id" %>'><%s x<|"designacao" %></a></td>
+        </tr>
+        <% end; %>
+      </tbody>
+    </table>
+  </div>
+
+let prog_template (prog: data list) ents projs =
+  General.navbar_inpage "Entidade" ^
+  <div class="top-left" style="position: absolute; top: 5em; left: 4em; font-size: 18px">
+    <% prog |> List.hd |> begin fun x -> %>
+    <h1><%s x<|"designacao" %></h1>
+    <p style="margin-bottom: 2rem;"></p>
+    <% end; %>
+    </div>
+  </div>
+
+  <div class="top-left2" style="position: absolute; top: 14em; left: 5em;">
+    <h2 style="margin-bottom: 1rem;">Entidades Associadas</h2>
+    <div style="width: 24rem" ;>
+      <table class="table table-dark table-hover">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Designação</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <% ents |> List.iter begin fun x -> %>
+          <tr>
+            <th scope="row"><%s x <| "id" %></th>
+            <td><%s x <| "nome" %></td>
+          </tr>
+          <% end; %>
+        </tbody>
+      </table>
+    </div>
+    <p style="margin-bottom: 2rem;"></p>
+  </div>
+
+  <div class="centre" style="position: absolute; top: 14em; left: 61em;">
+    <h2 style="margin-bottom: 1rem;">Projetos Financiados</h2>
+    <div style="width: 24rem;">
+      <table class="table table-dark table-hover">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <% projs |> List.iter begin fun x -> %>
+          <tr>
+            <th scope="row"><%s x<|"id"%></th>
+            <td><a href='/projetos/<%s x<|"id" %>'><%s x<|"nome"%></td>
+          </tr>
+          <% end; %>
+        </tbody>
+      </table>
+    </div>
+    <p style="margin-bottom: 2rem;"></p>
+  </div>
+
+  <div class="centre" style="position: absolute; top: 14em; left: 120em;">
+    <h2>Classificação</h2>
+    <p style="margin-bottom: 2rem;"></p>
+    PLACEHOLDER
+  </div>
+
+  <div class="d-grid gap-2 col-1 mx-auto" style="width: 3rem; position: absolute; top: 5em; right: 6em">
+    <a href="/index.html" class="btn btn-secondary" tabindex="-1" role="button" aria-disabled="true">
+    Modificar
+    </a>
+  </div>
+
---
+++ proj/src/templates/unid.eml.ml
@@ -0,0 +1,58 @@
+open Types
+
+let unidades (_unid: data list) =
+  General.navbar_home "Unidades" ^
+  <div style="text-align: center; width: 1000px; margin: 0 auto; border-style: none; margin-top: 30px;">
+    <table class="table table-dark table-hover">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Designação</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider">
+        <% _unid |> List.iter begin fun x -> %> 
+        <tr>
+          <th scope="row"><%s x<|"id" %></th>
+          <td><a href='/unidades/<%s x<|"id" %>'><%s x<|"nome" %></a></td>
+        </tr>
+        <% end; %>
+      </tbody>
+    </table>
+  </div>
+
+
+
+let unid_template (unid : data list) = 
+  General.navbar_inpage "Unidades" ^
+  <div class="top-left" style="position: absolute; top: 5em; left: 4em; font-size: 18px">
+    <h1><%s unid |> List.hd <| "Unome" %></h1>
+    <p style="margin-bottom: 2rem;"></p>
+
+    <h2 style="margin-bottom: 1rem;">Docentes</h2>
+    <div style="width: 24rem" ;>
+      <table class="table table-dark table-hover">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <% unid |> List.iter begin fun x -> %> 
+          <tr>
+            <th scope="row"><%s x<|"id" %></th>
+            <td><a href='/investigadores/<%s x<|"id" %>'><%s x<|"Inome" %></a></td>
+          </tr>
+          <% end; %>
+        </tbody>
+      </table>
+    </div>
+    <p style="margin-bottom: 2rem;"></p>
+  </div>
+
+  <div class="d-grid gap-2 col-1 mx-auto" style="width: 3rem; position: absolute; top: 5em; right: 6em">
+    <a href="/index.html" class="btn btn-secondary" tabindex="-1" role="button" aria-disabled="true">
+      Modificar
+    </a>
+  </div>
